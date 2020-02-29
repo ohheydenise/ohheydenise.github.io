5f264919d5f6bb229804baf8d8f774807fcd010b
ohheydenise <zacharydwilliams5@gmail.com>
2020-02-29 15:53:51 -0700

    Javascript

--- weather/js/hamMenu.js
+++ weather/js/hamMenu.js
@@ -3,3 +3,7 @@ function onClickMenu() {
     document.getElementById('nav').classList.toggle('change');
     document.getElementById('menu-bg').classList.toggle('change-bg');
 }
+
+const todaysdate = new Date();
+const options = {weekdays: 'long', day: 'numeric', month: 'long', year: 'numeric'};
+document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-US', options);
